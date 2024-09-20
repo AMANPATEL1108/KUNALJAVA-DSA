71dccea489883a95e4074db9c7fd2df2b36739f6
AMANPATEL <amanpatel8844382@gmail.com>
2024-09-20 11:51:51 +0530

    Comment Other Code

--- FirstJava/First.java
+++ FirstJava/First.java
@@ -5,7 +5,7 @@ public class First {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        System.out.println(args[1]);
+//        System.out.println(args[1]);
 
     }
 }
