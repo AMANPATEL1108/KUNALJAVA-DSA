cf3740aa3f036f4c1042a1d621e53b27c64ea936
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-03 17:03:36 +0530

    Binary Search Interview Questions - Google, Facebook, Amazon  Completed

---
+++ Binary_Search_Interview_Question_Google_Facebook_Amazone/src/SmallestLetter.java
@@ -0,0 +1,14 @@
+public class NextGreatestLetter {
+    public static void main(String[] args) {
+        int[] arr={'c','d','g','j'};
+        int target='c';
+        int ans=FindGreatestLetter(arr,target);
+
+    }
+    static char FindGreatestLetter(int[] arr, int target) {
+
+    }
+
+
+
+}
