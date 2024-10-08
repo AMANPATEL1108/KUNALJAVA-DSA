032bdc3fb845259478f174f1d408250fe68bc991
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-08 19:12:57 +0530

    Build Array from Permutation Completed from Array assignment

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/Permutation.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/Permutation.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Permutation {
+import java.util.Arrays;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        int[] newans = buildArray(nums);
+        System.out.println(Arrays.toString(newans));
+    }
+
+    static int[] buildArray(int[] nums) {
+        int sz = nums.length;
+        int[] ans = new int[sz];
+
+
+        for (int i = 0; i < sz; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
 }
