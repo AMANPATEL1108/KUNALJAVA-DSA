1ce5b0a1cffff626f6896a6209fa70cf3512a582
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-09 17:30:44 +0530

    Shuffle the Array

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/ShuffletheArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/ShuffletheArray.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class ShuffletheArray {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ShuffletheArray {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4,5,6};
+        int n=3;
+        int[] ans=shuffle(nums,n);
+        System.out.println(Arrays.toString(ans));
+    }
+
+     static int[] shuffle(int[] nums, int n) {
+        int result[]=new int[nums.length];
+        int indx=0;
+
+        for(int i=0;i<n;i++) {
+
+                result[indx++] = nums[i];
+                result[indx++] = nums[i+n];
+        }
+        return result;
+    }
 }
