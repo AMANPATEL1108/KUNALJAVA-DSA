729524e41721611da2d3a1808ea23905e419f019
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-10 15:47:38 +0530

    Number of Good Pairs Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/NumberofGoodPairs.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/NumberofGoodPairs.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class NumberofGoodPairs {
+public class NumberofGoodPairs {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,1,1,3};
+        int ans=numIdenticalPairs(nums);
+        System.out.println(ans);
+
+    }
+    static int numIdenticalPairs(int[] nums) {
+        int res=0;
+        int[] CountArray=new int[101];
+        for(int num:nums){
+            CountArray[num]++;
+        }
+        for(int Count:CountArray){
+            Count=(Count*(Count-1))/2;
+            res+=Count;
+        }
+        return res;
+    }
 }
