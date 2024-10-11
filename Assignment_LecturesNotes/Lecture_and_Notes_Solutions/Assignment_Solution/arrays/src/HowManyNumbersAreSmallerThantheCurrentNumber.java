7c2d2318984eecd5ba03fc49a9f57ca079447ba1
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-11 08:36:56 +0530

    HowManyNumbersAreSmallerThantheCurrentNumber Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/HowManyNumbersAreSmallerThantheCurrentNumber.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/HowManyNumbersAreSmallerThantheCurrentNumber.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class HowManyNumbersAreSmallerThantheCurrentNumber {
+import java.util.Arrays;
+
+public class HowManyNumbersAreSmallerThantheCurrentNumber {
+    public static void main(String[] args) {
+        int[] nums={6,5,4,8};
+        int[] ans=smallerNumbersThanCurrent(nums);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int arrlen=nums.length;
+        int count=0;
+        int[] ans=new int[arrlen];
+
+
+        for(int i=0;i<arrlen;i++){
+            count=0;
+            for(int j=0;j<arrlen;j++){
+                if (nums[i]>nums[j]){
+                    count++;
+                }
+                ans[i]=count;
+            }
+
+        }
+        return ans;
+
+    }
 }
