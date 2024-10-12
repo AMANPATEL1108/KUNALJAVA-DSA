8f040638ea7a1b7c481d5470dbf15f3f161ee5bd
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-12 10:26:01 +0530

    Create Target Array in the Given Order Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/CreateTargetArrayintheGivenOrder.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/CreateTargetArrayintheGivenOrder.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CreateTargetArrayintheGivenOrder {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateTargetArrayintheGivenOrder {
+    public static void main(String[] args) {
+        int[] nums={0,1,2,3,4};
+        int[] index={0,1,2,2,1};
+        int[] ans=createTargetArray(nums,index);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    static int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> target = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            target.add(index[i], nums[i]);
+        }
+
+        int[] result = new int[target.size()];
+        for (int i = 0; i < target.size(); i++) {
+            result[i] = target.get(i);
+        }
+        return result;
+    }
 }
