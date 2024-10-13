3b01cc224ba3e61e1a9160128d83c1d2313b42e5
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-13 08:14:53 +0530

    Check if the Sentence Is Pangram Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/CheckSentencePangram.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/CheckSentencePangram.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class CheckSentencePangram {
+public class CheckSentencePangram {
+    public static void main(String[] args) {
+        String a="leetcode";
+        System.out.println(checkIfPangram(a));
+    }
+
+    static boolean checkIfPangram(String sentence) {
+        for(char ch='a';ch<='z';ch++){
+            if (sentence.indexOf(ch)<0){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
