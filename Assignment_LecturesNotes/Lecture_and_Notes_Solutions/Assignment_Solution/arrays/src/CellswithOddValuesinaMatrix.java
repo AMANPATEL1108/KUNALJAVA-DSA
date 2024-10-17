d849458848094879e5b868396d143f3bb4afd64b
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-17 09:53:56 +0530

    Cells with Odd Values in a Matrix Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/CellswithOddValuesinaMatrix.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/CellswithOddValuesinaMatrix.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class CellswithOddValuesinaMatrix {
+import java.util.Arrays;
+
+public class CellswithOddValuesinaMatrix {
+    public static void main(String[] args) {
+        int[][] array={{0,1},{1,1}};
+        int m=2;
+        int n=3;
+        System.out.println(oddCells(m,n,array));
+    }
+
+    static int oddCells(int m, int n, int[][] indices) {
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < indices.length; i++) {
+            int row = indices[i][0];
+            int col = indices[i][1];
+
+            for (int j = 0; j < n; j++) {
+                matrix[row][j]++;
+            }
+
+            for (int j = 0; j < m; j++) {
+                matrix[j][col]++;
+            }
+        }
+
+
+
+        int oddCount = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] % 2 != 0) {
+                    oddCount++;
+                }
+            }
+        }
+
+        return oddCount;
+    }
 }
