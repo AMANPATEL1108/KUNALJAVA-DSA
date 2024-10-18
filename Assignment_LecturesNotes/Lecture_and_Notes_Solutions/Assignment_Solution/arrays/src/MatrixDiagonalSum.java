f9b887c232e2d1499b6144bc4ae46128bd609734
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-18 07:35:58 +0530

    Matrix Diagonal Sum Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MatrixDiagonalSum.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MatrixDiagonalSum.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MatrixDiagonalSum {
+public class MatrixDiagonalSum {
+    public static void main(String[] args) {
+        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        int ans=diagonalSum(matrix);
+        System.out.println(ans);
+    }
+    static int diagonalSum(int[][] mat) {
+        int sum=0;
+        int n = mat.length;
+        for(int i = 0; i <mat.length; i++){
+            sum += mat[i][i];
+            if (i != n - 1 - i) {
+                sum += mat[i][n - 1 - i];
+            }
+        }
+
+        return sum;
+    }
 }
