db17df9afa9b7fe81221ef6165243069487a1790
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-19 18:59:01 +0530

    Already Completed this :Find Numbers with Even Number of Digits,Completed this :Transpose Matrix

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FindNumberswithEvenNumberofDigits.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FindNumberswithEvenNumberofDigits.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class FindNumberswithEvenNumberofDigits {
+public class FindNumberswithEvenNumberofDigits {
+    public static void main(String[] args) {
+        int[] nums={12,345,2,6,7896};
+        System.out.println(findNumbers(nums));
+
+    }
+    static int findNumbers(int[] nums) {
+        int count=0;
+
+        for(int num:nums){
+            if (even(num)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    static boolean even(int num) {
+        int numberofDigits=digits(num);
+//        if (numberofDigits%2==0){
+//            return true;
+//        }
+        return numberofDigits%2==0;
+
+    }
+    //count numbers
+    static  int digits(int num){
+        if (num<0){
+            num=num *-1;
+        }
+        if(num==0){
+            return 1;
+        }
+        return (int)(Math.log10(num))+1;
+    }
 }
--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/TransposeMatrix.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/TransposeMatrix.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TransposeMatrix {
+import java.util.Arrays;
+
+    public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+        int[][] ans=transpose(arr);
+        System.out.println(Arrays.deepToString(ans));
+    }
+    static int[][] transpose(int[][] arr) {
+        int newarr[][]=new int[arr[0].length][arr.length];
+
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col< arr[0].length;col++){
+               newarr[col][row]=arr[row][col];
+            }
+        }
+        return newarr;
+    }
 }
