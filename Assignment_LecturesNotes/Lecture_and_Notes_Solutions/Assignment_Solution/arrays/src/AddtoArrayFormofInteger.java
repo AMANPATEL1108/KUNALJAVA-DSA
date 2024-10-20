c8d7880ceaf7be8742787fdbe2ff58b24ac0aae6
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-20 10:13:31 +0530

    Add to Array-Form of Integer,Maximum Population Year Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/AddtoArrayFormofInteger.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/AddtoArrayFormofInteger.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class AddtoArrayFormofInteger {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddtoArrayFormofInteger {
+    public static void main(String[] args) {
+        int[] num={1,2,1,0};
+        int k=32;
+        List<Integer> ans=addToArrayForm(num,k);
+        System.out.println(ans);
+    }
+    static List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+
+        int i = num.length - 1;
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                k += num[i];
+            }
+
+            result.add(0, k % 10);
+            k /= 10;
+
+            i--;
+        }
+
+        return result;
+    }
 }
--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MaximumPopulationYear.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MaximumPopulationYear.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class MaximumPopulationYear {
+public class MaximumPopulationYear {
+    public static void main(String[] args) {
+    int[][] logs={{1993,1999},{2000,2010}};
+    int ans=maximumPopulation(logs);
+        System.out.println(ans);
+    }
+    static int maximumPopulation(int[][] logs) {
+        int[] arr = new int[101];
+        for(int[] log : logs) {
+            int birth = log[0];
+            int death = log[1];
+            arr[birth-1950]++;
+            arr[death-1950]--;
+        }
+        int max = arr[0];
+        int year = 1950;
+        for(int i = 1 ; i < 101; i++) {
+            arr[i] += arr[i-1];
+            if(arr[i] > max) {
+                max = arr[i];
+                year = 1950 + i;
+            }
+        }
+        return year;
+    }
 }
