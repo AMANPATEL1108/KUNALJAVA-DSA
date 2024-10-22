160cdd69e4b30d5b1275c9a1ca1a06d5c99a7e03
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-22 07:43:00 +0530

    Find N Unique Integers Sum up to Zero Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FindNUniqueIntegersSumuptoZero.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FindNUniqueIntegersSumuptoZero.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class FindNUniqueIntegersSumuptoZero {
+import java.util.Arrays;
+import java.util.Random;
+
+public class FindNUniqueIntegersSumuptoZero {
+    public static void main(String[] args) {
+        int n = 9;
+        int[] ans = sumZero(n);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] sumZero(int n) {
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n / 2; i++) {
+            arr[i] = i + 1;
+            arr[n - 1 - i] = -(i + 1);
+        }
+
+        if (n % 2 != 0) {
+            arr[n / 2] = 0;
+        }
+
+        return arr;
+    }
 }
--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/TwoSum.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/TwoSum.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class TwoSum {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[]  nums={2,7,11,15};
+        int target=9;
+
+        int[] ans=twoSum(nums,target);
+        System.out.println(Arrays.toString(ans));
+
+
+    }
+    static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> visited = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer j = visited.get(target - nums[i]);
+            if (j != null) {
+                return new int[] { j, i };
+            }
+            visited.put(nums[i], i);
+        }   return nums;
+    }
 }
