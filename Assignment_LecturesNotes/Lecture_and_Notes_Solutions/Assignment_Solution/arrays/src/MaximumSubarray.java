034abb5ed238eb702dccc1eed424d746897b2c8d
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-23 15:49:00 +0530

    Maximum Subarray Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MaximumSubarray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MaximumSubarray.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MaximumSubarray {
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] anums={-2,1,-3,4,-1,2,1,-5,4};
+        int ans=maxSubArray(anums);
+        System.out.println(ans);
+    }
+
+    static int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int maxEnding = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            maxEnding = Math.max(maxEnding + nums[i], nums[i]);
+
+            res = Math.max(res, maxEnding);
+        }
+        return res;
+    }
 }
