57df5132e0990b60d289d605ce86324abd7bd2e2
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-25 08:17:36 +0530

    Plus One Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/PlusOne.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/PlusOne.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class PlusOne {
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        int[] ans = plusOne(digits);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] arr = new int[digits.length + 1];
+        arr[0] = 1;
+        return arr;
+    }
 }
