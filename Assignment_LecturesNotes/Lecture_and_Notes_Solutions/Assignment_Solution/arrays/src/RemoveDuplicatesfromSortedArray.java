f43d5aa63725cdf663d5d022760d5a217ae7b6fa
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-26 07:48:40 +0530

    Remove Duplicates from Sorted Array Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/RemoveDuplicatesfromSortedArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/RemoveDuplicatesfromSortedArray.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class RemoveDuplicatesfromSortedArray {
+import java.util.Arrays;
+
+public class RemoveDuplicatesfromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 1, 2};
+        int ans = removeDuplicates(nums);
+        System.out.println(ans);
+    }
+
+    static int removeDuplicates(int[] nums) {
+        bubble(nums);
+
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+
+    static int[] bubble(int[] nums) {
+        boolean swapped;
+        for (int i = 0; i < nums.length; i++) {
+            swapped = false;
+            for (int j = 1; j < nums.length - i; j++) {
+                if (nums[j] < nums[j - 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        return nums;
+    }
 }
