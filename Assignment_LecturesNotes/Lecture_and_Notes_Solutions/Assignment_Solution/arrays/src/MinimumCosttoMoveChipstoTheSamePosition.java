2759e13dbb2f8eb6cc214cab34227c272c59eb45
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-27 06:52:05 +0530

    Minimum Cost to Move Chips to The Same Position Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MinimumCosttoMoveChipstoTheSamePosition.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MinimumCosttoMoveChipstoTheSamePosition.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class MinimumCosttoMoveChipstoTheSamePosition {
+public class MinimumCosttoMoveChipstoTheSamePosition {
+
+    public static void main(String[] args) {
+        int[] Position={1,2,3};
+        int ans=minCostToMoveChips(Position);
+        System.out.println(ans);
+
+    }
+    static int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+        for(int chips : position) {
+            if(chips % 2 == 0) {
+                even++;
+            }
+            else {
+                odd++;
+            }
+        }
+        return Math.min(even, odd);
+    }
 }
+
