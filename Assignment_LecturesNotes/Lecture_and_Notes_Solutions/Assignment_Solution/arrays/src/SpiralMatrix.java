06db7bbb2dc50b8ba169b257d0fcbce3d00947ac
AMANPATEL <amanpatel8844382@gmail.com>
2024-10-29 07:10:25 +0530

    Spiral Matrix II Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/SpiralMatrix.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/SpiralMatrix.java
@@ -13,42 +13,42 @@ public class SpiralMatrix {
         System.out.println(ans);
     }
 
-    static List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ans = new ArrayList<>();
+        static List<Integer> spiralOrder(int[][] matrix) {
+            List<Integer> ans = new ArrayList<>();
 
-        if (matrix == null || matrix.length == 0) {
+            if (matrix == null || matrix.length == 0) {
+                return ans;
+            }
+
+            int top = 0, bottom = matrix.length - 1;
+            int left = 0, right = matrix[0].length - 1;
+
+            while (top <= bottom && left <= right) {
+                for (int j = left; j <= right; j++) {
+                    ans.add(matrix[top][j]);
+                }
+                top++;
+
+                for (int i = top; i <= bottom; i++) {
+                    ans.add(matrix[i][right]);
+                }
+                right--;
+
+                if (top <= bottom) {
+                    for (int j = right; j >= left; j--) {
+                        ans.add(matrix[bottom][j]);
+                    }
+                    bottom--;
+                }
+
+                if (left <= right) {
+                    for (int i = bottom; i >= top; i--) {
+                        ans.add(matrix[i][left]);
+                    }
+                    left++;
+                }
+            }
+
             return ans;
         }
-
-        int top = 0, bottom = matrix.length - 1;
-        int left = 0, right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-            for (int j = left; j <= right; j++) {
-                ans.add(matrix[top][j]);
-            }
-            top++;
-
-            for (int i = top; i <= bottom; i++) {
-                ans.add(matrix[i][right]);
-            }
-            right--;
-
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    ans.add(matrix[bottom][j]);
-                }
-                bottom--;
-            }
-
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans.add(matrix[i][left]);
-                }
-                left++;
-            }
-        }
-
-        return ans;
-    }
 }
--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/SpiralMatrixII.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/SpiralMatrixII.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class SpiralMatrixII {
+import java.util.Arrays;
+
+public class SpiralMatrixII {
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] result = generateMatrix(n);
+
+        for (int[] row : result) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+    static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+        int num = 1;
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; j++) {
+                matrix[top][j] = num++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    matrix[bottom][j] = num++;
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        return matrix;
+    }
 }
