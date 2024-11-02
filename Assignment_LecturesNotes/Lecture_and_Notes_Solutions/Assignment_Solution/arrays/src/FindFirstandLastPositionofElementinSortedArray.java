5642563b142bb7bb5d414fc003ee26083d6dc99b
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-02 07:09:38 +0530

    Find First and Last Position of Element in Sorted Array Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FindFirstandLastPositionofElementinSortedArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FindFirstandLastPositionofElementinSortedArray.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class FindFirstandLastPositionofElementinSortedArray {
+import java.util.Arrays;
+
+public class FindFirstandLastPositionofElementinSortedArray {
+
+    public static void main(String[] args) {
+
+        int[] nums={5,7,7,8,8,10};
+        int target=8;
+        int ans[]=searchRange(nums,target);
+        System.out.println(Arrays.toString(ans));
+
+    }
+
+    static int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[1] = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
 }
