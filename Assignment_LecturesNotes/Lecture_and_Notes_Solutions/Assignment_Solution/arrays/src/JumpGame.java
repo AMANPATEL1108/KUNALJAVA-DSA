80b8a186febd8987fb02789057f76a99074e466d
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-03 10:14:39 +0530

    Jump Game Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/JumpGame.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/JumpGame.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class JumpGame {
+public class JumpGame {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 1, 1, 4};
+        System.out.println(canJump(arr));
+    }
+
+    static boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
