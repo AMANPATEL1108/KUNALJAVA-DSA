22bd785892c8941fa43b8878d49a16dd61312bcf
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-04 07:58:49 +0530

    Rotate Array Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/RotateArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/RotateArray.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class RotateArray {
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7};
+        int k=3;
+        rotate(arr,k);
+    }
+
+    static void rotate(int[] nums, int k) {
+        k=k% nums.length;
+      reverse(nums,0, nums.length-1);
+      reverse(nums,0,k-1);
+      reverse(nums,k, nums.length-1);
+    }
+
+    static  void reverse(int[] nums,int start,int end){
+        while (start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
 }
