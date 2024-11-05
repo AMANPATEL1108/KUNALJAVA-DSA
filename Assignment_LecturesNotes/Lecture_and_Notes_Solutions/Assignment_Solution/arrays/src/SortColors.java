8d27c1235acd0cddb0eacbfb0a78ec796ee26e81
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-05 08:01:46 +0530

    Sort Colors Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/SortColors.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/SortColors.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class SortColors {
+import java.util.Arrays;
+
+public class SortColors {
+    public static void main(String[] args) {
+        int[] arr={2,0,2,1,1,0};
+        sortColors(arr);
+
+    }
+
+    static void sortColors(int[] nums) {
+        for(int j=0;j< nums.length;j++){
+        for (int i = 1; i <= nums.length-1; i++) {
+            if (nums[i] < nums[i - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+            }
+        }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
 }
