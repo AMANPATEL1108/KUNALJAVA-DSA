57d59d42c2d94e54e8129243db822ea3b26e511d
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-07 08:03:48 +0530

    Max Value of Equation Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MaxValueofEquation.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/MaxValueofEquation.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class MaxValueofEquation {
+public class MaxValueofEquation {
+    public static void main(String[] args) {
+        int[][] arr={{1,3},{2,0},{5,10},{6,-10}};
+        int k=1;
+        int ans=findMaxValueOfEquation(arr,k);
+        System.out.println(ans);
+    }
+
+    static int findMaxValueOfEquation(int[][] points, int k) {
+        int ans=Integer.MIN_VALUE;
+        int i=0;
+        int f=1;
+        while(i < points.length) {
+            if(f<i+1)
+                f=i+1;
+            for (int j = f; j <= points.length-1; j++) {
+                if(points[j][0]>(points[i][0]+k))
+                    break;
+                if((points[i][1]+points[j][1]+points[j][0]-points[i][0])>ans){
+                    ans=points[i][1]+points[j][1]+points[j][0]-points[i][0];
+                    f=j-1;
+                }
+            }
+            i++;
+        }
+        return ans;
+    }
+
 }
