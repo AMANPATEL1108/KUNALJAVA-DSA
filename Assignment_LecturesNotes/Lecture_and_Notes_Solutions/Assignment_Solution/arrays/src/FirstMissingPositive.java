8326ea0e85b7eeea993f15a9bb3263cd140b73ed
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-08 07:52:11 +0530

    First Missing Positive Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FirstMissingPositive.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/arrays/src/FirstMissingPositive.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class FirstMissingPositive {
+import java.util.Arrays;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0};
+        int ans = firstMissingPositive(arr);
+        System.out.println(ans);
+    }
+
+    static int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0 && nums[i] == ans + 1) {
+                ans = nums[i];
+            }
+        }
+
+        return ans + 1;
+    }
 }
