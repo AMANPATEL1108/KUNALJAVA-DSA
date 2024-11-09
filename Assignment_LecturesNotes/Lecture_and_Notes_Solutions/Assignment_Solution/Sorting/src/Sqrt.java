1f2639a1b333545b3aa6918b052b1a91ed56b67e
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-09 07:44:14 +0530

    Square Root Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/Sqrt.java
@@ -0,0 +1,11 @@
+public class Sqrt {
+    public static void main(String[] args) {
+        int x=8;
+        System.out.println(mySqrt(4));
+    }
+
+    static int mySqrt(int x) {
+        int sqrt = (int)Math.sqrt(x);
+        return sqrt;
+    }
+}
