937f8d758cb95136369278fcbf14bd5be22563ba
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-25 07:03:25 +0530

    Search in Rotated Sorted Array II Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/SearchinRotatedSortedArrayII.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/SearchinRotatedSortedArrayII.java
@@ -1,6 +1,37 @@
-public class SearchinRotatedSortedArray {
+public class SearchinRotatedSortedArrayII {
     public static void main(String[] args) {
+    int[] arr={2,5,6,0,0,1,2};
+    int target=0;
 
+        System.out.println(search(arr,target));
     }
-    
+    static boolean search(int[] nums, int target) {
+        int pivot = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] > nums[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
+        int a = binarySearch(nums, 0 , pivot, target);
+        if(a >= 0)
+            return true;
+        a = binarySearch(nums, pivot+1, nums.length - 1, target);
+        if(a > 0)
+            return true;
+        return false;
+    }
+    static int binarySearch(int nums[], int l, int r, int target) {
+
+        while(l <= r) {
+            int mid = (l + r)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
+    }
 }
