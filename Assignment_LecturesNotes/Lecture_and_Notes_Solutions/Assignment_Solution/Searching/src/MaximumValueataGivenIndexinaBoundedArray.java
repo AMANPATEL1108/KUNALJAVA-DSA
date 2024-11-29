0e3c3f05ab714c1d39282f056c11e04bc2a07e20
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-29 07:57:04 +0530

    Maximum Value at a Given Index in a Bounded Array Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/MaximumValueataGivenIndexinaBoundedArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/MaximumValueataGivenIndexinaBoundedArray.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class MaximumValueataGivenIndexinaBoundedArray {
+public class MaximumValueataGivenIndexinaBoundedArray {
+    public static void main(String[] args) {
+        int n=4;
+        int index=2;
+        int maxSum=6;
+
+        System.out.println(maxValue(n,index,maxSum));
+
+    }
+    static int maxValue(int n, int index, int maxSum) {
+        int res=1;
+        maxSum -=n;
+        int left=0,right=0;
+        int maxLeft=index,maxRight=n-index-1;
+
+        while (maxSum>0){
+            res++;
+            int leftVal=Math.min(left++,maxLeft);
+            int rightVal=Math.min(right++,maxRight);
+            maxSum-=(1+leftVal+rightVal);
+
+            if (leftVal==maxLeft && rightVal==maxRight){
+                break;
+            }
+
+        }
+        if (maxSum>0){
+            res=res+(maxSum/n);
+
+        }
+        return (maxSum<0)?res-1:res;
+    }
 }
