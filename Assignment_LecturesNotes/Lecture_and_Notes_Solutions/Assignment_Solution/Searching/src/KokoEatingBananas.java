2a079bb91b66c7025bff63638d0908ef04fcb686
AMANPATEL <amanpatel8844382@gmail.com>
2024-11-30 07:28:52 +0530

    Koko Eating Bananas Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/KokoEatingBananas.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/KokoEatingBananas.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class KokoEatingBananas {
+public class KokoEatingBananas {
+    public static void main(String[] args) {
+        int[] piles={3,6,7,11};
+        int h=8;
+        System.out.println(minEatingSpeed(piles,8));
+
+    }
+    static int minEatingSpeed(int[] piles, int h) {
+        int i = 1;
+        int j = Integer.MIN_VALUE;
+        for (int pile : piles) {
+            j = Math.max(j, pile);
+        }
+
+        while (i < j) {
+            int k = i + (j - i) / 2;
+            int time = 0;
+            for (int pile : piles) {
+                time += (pile + k - 1) / k;
+            }
+
+            if (time > h) {
+                i = k + 1;
+            } else {
+                j = k;
+            }
+        }
+
+        return i;
+    }
+
 }
