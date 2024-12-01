16939555eb1a53223db0053cdd39c06f1f7b92b5
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-01 07:51:57 +0530

    Minimum Absolute Sum Difference Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/MinimumAbsoluteSumDifference.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/MinimumAbsoluteSumDifference.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class MinimumAbsoluteSumDifference {
+import java.util.Arrays;
+
+public class MinimumAbsoluteSumDifference {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 10, 4, 4, 2, 7};
+        int[] nums2 = {9, 3, 5, 1, 7, 4};
+        System.out.println(minAbsoluteSumDiff(nums1, nums2));
+    }
+
+    static int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
+        int mod = (int) 1e9 + 7;
+        int replace = 0;
+        int save;
+        int currDiff;
+        int sumDiff = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            currDiff = Math.abs(nums1[i] - nums2[i]);
+            sumDiff = (sumDiff + currDiff) % mod;
+            if (replace < currDiff) {
+                for (int j : nums1) {
+                    save = currDiff - Math.abs(j - nums2[i]);
+                    if (save > replace) {
+                        replace = save;
+                    }
+                    if (replace == currDiff) {
+                        break;
+                    }
+                }
+            }
+        }
+        return (sumDiff - replace + mod) % mod;
+    }
 }
