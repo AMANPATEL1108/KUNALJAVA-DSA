0435f72f2ab7c3191a3a3d84038bd37034157e8f
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-02 06:41:51 +0530

    Search a 2D Matrix Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/Searcha2DMatrix.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/Searcha2DMatrix.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Searcha2DMatrix {
+public class Searcha2DMatrix {
+    public static void main(String[] args) {
+    int[][] arr={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    int target=3;
+        System.out.println(searchMatrix(arr,target));
+    }
+
+    static boolean searchMatrix(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
