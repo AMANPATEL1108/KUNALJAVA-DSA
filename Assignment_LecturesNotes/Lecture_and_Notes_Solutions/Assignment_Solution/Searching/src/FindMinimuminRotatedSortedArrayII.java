9a0fff98ad96e0920bdef40284e68a3c7c182585
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-06 07:25:45 +0530

    Find Minimum in Rotated Sorted Array II Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/FindMinimuminRotatedSortedArrayII.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/FindMinimuminRotatedSortedArrayII.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class FindMinimuminRotatedSortedArrayII {
+public class FindMinimuminRotatedSortedArrayII {
+    public static void main(String[] args) {
+        int[] arr={1,3,5};
+        System.out.println(findMin(arr));
+    }
+    static int findMin(int[] nums) {
+        int min=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if (min>nums[i]){
+                min=nums[i];
+            }
+        }
+        return min;
+    }
 }
