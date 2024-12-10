de079b256edeb32f0bc1d3928174de710edb8192
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-10 09:25:58 +0530

    Count smaller number after Self Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/CountsmallernumberafterSelf.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/CountsmallernumberafterSelf.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class CountsmallernumberafterSelf {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CountsmallernumberafterSelf {
+    public static void main(String[] args) {
+        int[] arr={5,2,6,1};
+        System.out.println(countSmaller(arr));
+
+    }
+    static List<Integer> countSmaller(int[] nums) {
+        int len = nums.length;
+        List<Integer> ans = new ArrayList<Integer>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+
+        for(int num: nums) arr.add(num);
+
+        Collections.sort(arr);
+
+        for(int i = 0; i<len; i++){
+            int index = binarySearch(arr,nums[i]);
+            ans.add(index);
+            arr.remove(index);
+        }
+
+        return ans;
+    }
+
+    static int binarySearch(ArrayList<Integer> arr, int target){
+        int start = 0;
+        int end = arr.size()-1;
+        int mid=0;
+
+        while(start<=end){
+            mid = start + ((end - start)/2);
+            int val = arr.get(mid);
+            if(val<target) start = mid + 1;
+            else end = mid - 1;
+        }
+        if(arr.get(start) == target) return start;
+        return mid;
+    }
 }
--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/FindinMountainArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Searching/src/FindinMountainArray.java
@@ -1,57 +1,57 @@
-public class FindinMountainArray {
-    public static void main(String[] args) {
-    int[] MountainArr={1,2,3,4,5,3,1};
-    int target=3;
-    findInMountainArray(target,MountainArr);
-    }
-    static int findInMountainArray(int target, MountainArray mountainArr) {
-        int n=mountainArr.length();
-        int low=1;
-        int high=n-2;
-        while(low!=high){
-            int mid=low+(high-low)/2;
-            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
-                low=mid+1;
-            }else{
-                high=mid;
-            }
-        }
-        int peak=low;
-        low=0;
-        high=peak;
-
-        while(low!=high){
-            int mid=low+(high-low)/2;
-            int curr=mountainArr.get(mid);
-            if(curr==target){
-                return mid;
-            }
-            if(curr<target){
-                low=mid+1;
-            }else{
-                high=mid;
-            }
-        }
-
-        if(mountainArr.get(low)==target) return low;
-
-        low=peak+1;
-        high=n-1;
-
-        while(low!=high){
-            int mid=low+(high-low)/2;
-            int curr=mountainArr.get(mid);
-            if(curr==target){
-                return mid;
-            }
-            if(curr>target){
-                low=mid+1;
-            }else{
-                high=mid;
-            }
-
-        }
-        if(mountainArr.get(low)==target) return low;
-        return -1;
-    }
-}
+//public class FindinMountainArray {
+//    public static void main(String[] args) {
+//    int[] MountainArr={1,2,3,4,5,3,1};
+//    int target=3;
+//    findInMountainArray(target,MountainArr);
+//    }
+//    static int findInMountainArray(int target, MountainArray mountainArr) {
+//        int n=mountainArr.length();
+//        int low=1;
+//        int high=n-2;
+//        while(low!=high){
+//            int mid=low+(high-low)/2;
+//            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
+//                low=mid+1;
+//            }else{
+//                high=mid;
+//            }
+//        }
+//        int peak=low;
+//        low=0;
+//        high=peak;
+//
+//        while(low!=high){
+//            int mid=low+(high-low)/2;
+//            int curr=mountainArr.get(mid);
+//            if(curr==target){
+//                return mid;
+//            }
+//            if(curr<target){
+//                low=mid+1;
+//            }else{
+//                high=mid;
+//            }
+//        }
+//
+//        if(mountainArr.get(low)==target) return low;
+//
+//        low=peak+1;
+//        high=n-1;
+//
+//        while(low!=high){
+//            int mid=low+(high-low)/2;
+//            int curr=mountainArr.get(mid);
+//            if(curr==target){
+//                return mid;
+//            }
+//            if(curr>target){
+//                low=mid+1;
+//            }else{
+//                high=mid;
+//            }
+//
+//        }
+//        if(mountainArr.get(low)==target) return low;
+//        return -1;
+//    }
+//}
