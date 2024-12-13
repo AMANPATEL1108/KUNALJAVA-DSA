5ca74fb1b81cfb0aeb48ce50fd50bd2592134ba7
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-13 18:49:20 +0530

    Majority Element Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MajorityElement.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MajorityElement.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class MajorityElement {
+import java.util.Arrays;
+
+public class MajorityElement {
+        public static void main(String[] args) {
+    int[] arr={3,2,3};
+            System.out.println(majorityElement(arr));
+    }
+    static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+    }
 }
