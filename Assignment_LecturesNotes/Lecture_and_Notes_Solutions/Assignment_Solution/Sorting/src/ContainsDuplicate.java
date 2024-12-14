39d16a31b5f98107eccd46adbd6aa814e060f5ce
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-14 07:48:48 +0530

    Contains Duplicate Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/ContainsDuplicate.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/ContainsDuplicate.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ContainsDuplicate {
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,1};
+        System.out.println(containsDuplicate(arr));
+    }
+
+    static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for(int i=1;i< nums.length;i++){
+            if (nums[i-1]==nums[i]){
+                return true;
+            }
+        }
+        return false;
+    }
 }
