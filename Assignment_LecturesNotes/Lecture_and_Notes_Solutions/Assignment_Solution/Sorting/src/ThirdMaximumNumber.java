d92633d4307895ff6817cd396c6e10ba406c98e6
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-15 07:38:56 +0530

    Third Maximum Number Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/ThirdMaximumNumber.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/ThirdMaximumNumber.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ThirdMaximumNumber {
+import java.util.Arrays;
+
+public class ThirdMaximumNumber {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1};
+        System.out.println(thirdMax(arr));
+    }
+
+    static int thirdMax(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        if (n < 3) {
+            return nums[n - 1];
+        }
+
+        int count = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] != nums[i + 1]) {
+                count++;
+            }
+            if (count == 3) {
+                return nums[i];
+            }
+        }
+
+        return nums[n - 1];
+    }
 }
