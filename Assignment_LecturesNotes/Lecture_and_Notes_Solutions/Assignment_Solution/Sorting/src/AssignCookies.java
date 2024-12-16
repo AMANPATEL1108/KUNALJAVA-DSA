0586e0c46156bec8126a4b2ae9a5e9691f3a64d5
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-16 18:45:56 +0530

    Assign Cookies Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/AssignCookies.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/AssignCookies.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+
 public class AssignCookies {
+    public static void main(String[] args) {
+int[] g={1,2,3};
+int[] s={1,1};
+
+        System.out.println(findContentChildren(g,s));
+    }
+
+    static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0, j = 0, contentChildren = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                contentChildren++;
+                i++;
+            }
+            j++;
+
+        }
+
+        return contentChildren;
+    }
 }
