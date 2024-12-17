8ce02daac7d0528c59a9ba2078f737ce99028354
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-17 07:32:16 +0530

    Maximum Product of Three Numbers Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MaximumProductofThreeNumbers.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MaximumProductofThreeNumbers.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MaximumProductofThreeNumbers {
+import java.util.Arrays;
+
+public class MaximumProductofThreeNumbers {
+    public static void main(String[] args) {
+        int[] arr = {-100, -98, -1, 2, 3, 4};
+        System.out.println(maximumProduct(arr));
+    }
+
+    static int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        int product1 = nums[n-1] * nums[n-2] * nums[n-3];
+
+        int product2 = nums[0] * nums[1] * nums[n-1];
+
+        return Math.max(product1, product2);
+    }
 }
