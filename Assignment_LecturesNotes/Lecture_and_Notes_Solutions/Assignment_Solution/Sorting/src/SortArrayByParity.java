e06e12e2fec5650adfeca95e2964607dac63e7e9
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-18 07:07:18 +0530

    Sort Array By Parity Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SortArrayByParity.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SortArrayByParity.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SortArrayByParity {
+import java.util.Arrays;
+
+public class SortArrayByParity {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4};
+        System.out.println(Arrays.toString(sortArrayByParity(arr)));
+    }
+
+    static int[] sortArrayByParity(int[] nums) {
+        int[] ans = new int[nums.length];
+        int evenIndex = 0;
+        int oddIndex = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                ans[evenIndex++] = nums[i];
+            } else {
+                ans[oddIndex--] = nums[i];
+            }
+        }
+
+        return ans;
+    }
 }
