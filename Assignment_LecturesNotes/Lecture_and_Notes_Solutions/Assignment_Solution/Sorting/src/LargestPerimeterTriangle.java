a6cab9ec364fc426fdf5d7393c671422e8b94f08
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-20 06:55:49 +0530

    Largest Perimeter Triangle Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/LargestPerimeterTriangle.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/LargestPerimeterTriangle.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class LargestPerimeterTriangle {
+import java.util.Arrays;
+
+public class LargestPerimeterTriangle {
+    public static void main(String[] args) {
+int[] arr={2,1,2};
+        System.out.println(largestPerimeter(arr));
+    }
+
+    static int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            int a = nums[i - 2];
+            int b = nums[i - 1];
+            int c = nums[i];
+            if (a + b > c) {
+                return a + c + b ;
+            }
+        }
+        return 0;
+    }
 }
