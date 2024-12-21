4db406273809839e0b181f3e01f00ca0260e1e9f
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-21 07:09:06 +0530

    Squares of a Sorted Array Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SquaresofaSortedArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SquaresofaSortedArray.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class SquaresofaSortedArray {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class SquaresofaSortedArray {
+    public static void main(String[] args) {
+        int[] arr={ -4,-1,0,3,10};
+
+        System.out.println(Arrays.toString(sortedSquares(arr)));
+    }
+
+    static int[] sortedSquares(int[] nums) {
+
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+
+        Arrays.sort(nums);
+
+
+        return nums;
+    }
 }
