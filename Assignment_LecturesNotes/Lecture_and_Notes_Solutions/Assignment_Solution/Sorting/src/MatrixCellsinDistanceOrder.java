68e7ab2572bf688d31f1be20249cf4fea9ca8d24
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-22 07:31:44 +0530

    Matrix Cells in Distance Order Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MatrixCellsinDistanceOrder.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MatrixCellsinDistanceOrder.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class MatrixCellsinDistanceOrder {
+import java.util.Arrays;
+
+public class MatrixCellsinDistanceOrder {
+    public static void main(String[] args) {
+        int rows = 3;
+        int cols = 3;
+        int rCenter = 0;
+        int cCenter = 0;
+        int[][] ans=allCellsDistOrder(rows,cols,rCenter,cCenter);
+        System.out.println(Arrays.deepToString(ans));
+    }
+
+    static int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int[][] ans = new int[rows * cols][2];
+
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ans[index][0] = i;
+                ans[index][1] = j;
+                index++;
+            }
+        }
+
+        Arrays.sort(ans, (a, b) -> {
+            int distA = Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter);
+            int distB = Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter);
+            return Integer.compare(distA, distB);
+        });
+
+        return ans;
+    }
 }
--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/Practice_WordCount.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/Practice_WordCount.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Practice_WordCount {
+public class Practice_WordCount {
+
+    public static void main(String[] args) {
+        // Test the function with a sample sentence
+        String sentence = "    Hello world this is           Java        ";
+        countWordsAndStore(sentence);
+    }
+
+
+     static void countWordsAndStore(String sentence) {
+        String[] words = new String[100];
+        int wordCount = 0;
+        String currentWord = "";
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+            if (ch == ' ' || i == sentence.length() - 1) {
+                if (currentWord.length() > 0) {
+                    words[wordCount] = currentWord;
+                    wordCount++;
+                    currentWord = "";
+                }
+            } else {
+                currentWord += ch;
+            }
+        }
+
+        System.out.println("The number of words in the sentence: " + wordCount);
+
+
+    }
+
+
 }
