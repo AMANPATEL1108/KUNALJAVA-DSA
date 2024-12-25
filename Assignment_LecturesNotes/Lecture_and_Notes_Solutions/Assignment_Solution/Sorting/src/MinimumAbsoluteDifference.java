628f5e066a24111690359078a645850eba819c73
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-25 07:27:20 +0530

    Minimum Absolute Difference Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MinimumAbsoluteDifference.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/MinimumAbsoluteDifference.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MinimumAbsoluteDifference {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MinimumAbsoluteDifference {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 3};
+        List<List<Integer>> result = minimumAbsDifference(arr);
+        System.out.println(result);
+    }
+    static List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < min) {
+                min = diff;
+                res.clear();
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (diff == min) {
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+        return res;
+    }
 }
