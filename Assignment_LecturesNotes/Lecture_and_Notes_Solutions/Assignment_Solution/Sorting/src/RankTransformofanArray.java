86849ed8727289a7e845052e43baec987f4bcbf8
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-26 08:15:03 +0530

    Rank Transform of an Array Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/RankTransformofanArray.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/RankTransformofanArray.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RankTransformofanArray {
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class RankTransformofanArray {
+    public static void main(String[] args) {
+        int[] arr={37,12,28,9,100,56,80,5,12};
+        int[] ans=arrayRankTransform(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] arrayRankTransform(int[] arr) {
+        int[] A = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(A);
+        System.out.println(Arrays.toString(A));
+
+        HashMap<Integer, Integer> rank = new HashMap<>();
+        for (int x : A)
+            rank.putIfAbsent(x, rank.size() + 1);
+        for (int i = 0; i < arr.length; ++i)
+            A[i] = rank.get(arr[i]);
+        return A;
+        }
 }
