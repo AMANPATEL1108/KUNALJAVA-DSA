a7cec7eaeac43b707b19495222da8de04062281d
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-27 08:40:02 +0530

    Sort Integers by The Number of 1 Bits Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SortIntegersbyTheNumberof1Bits.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SortIntegersbyTheNumberof1Bits.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SortIntegersbyTheNumberof1Bits {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SortIntegersbyTheNumberof1Bits {
+    public static void main(String[] args) {
+int[] arr={0,1,2,3,4,5,6,7,8};
+int[] ans=sortByBits(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int[] sortByBits(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]+=Integer.bitCount(arr[i])*10001;
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=arr[i]%10001;
+        }
+        return arr;
+    }
 }
