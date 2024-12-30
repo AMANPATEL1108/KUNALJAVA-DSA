51b46826aaa18c670c60a6c28a10d16710b40e55
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-30 07:29:13 +0530

    Average Salary Excluding the Minimum and Maximum Salary Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/AverageSalaryExcludingtheMinimumandMaximumSalary.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/AverageSalaryExcludingtheMinimumandMaximumSalary.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class AverageSalaryExcludingtheMinimumandMaximumSalary {
-}
+public class AverageSalaryExcludingtheMinimumandMaximumSalary {
+    public static void main(String[] args) {
+        int[] a={4000,3000,1000,2000};
+
+        System.out.println(average(a));
+    }
+    static double average(int[] salary) {
+                double sum = 0;
+                int minSalary = Integer.MAX_VALUE;
+                int maxSalary = Integer.MIN_VALUE;
+
+                for (int i = 0; i < salary.length; i++) {
+                    sum += salary[i];
+                    if (salary[i] < minSalary) {
+                        minSalary = salary[i];
+                    }
+                    if (salary[i] > maxSalary) {
+                        maxSalary = salary[i];
+                    }
+                }
+
+                sum -= (minSalary + maxSalary);
+                double sal = salary.length - 2;
+                double total = sum / sal;
+
+                return total;
+            }
+        }
