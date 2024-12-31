d70b103c7fd8cf002e048b0330d6f95cb150d78a
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-31 07:52:38 +0530

    Can Make Arithmetic Progression From Sequence Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/CanMakeArithmeticProgressionFromSequence.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/CanMakeArithmeticProgressionFromSequence.java
@@ -1,6 +1,21 @@
-public class MakeTwoArraysEqualbyReversingSubarrays {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class CanMakeArithmeticProgressionFromSequence {
+    public static void main(String[] args) {
+    int[] arr={3,5,1};
+        System.out.println(canMakeArithmeticProgression(arr));
     }
+    static boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int d=arr[1]-arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(d!=arr[i]-arr[i-1]){
+                return  false;
+            }
+        }
+
+        return true;
+    }
+
 
 }
