d012f25d394ff0706209e62c11da9cc6fd891b43
AMANPATEL <amanpatel8844382@gmail.com>
2025-01-01 07:32:32 +0530

    Sort Array by Increasing Frequency Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SortArraybyIncreasingFrequency.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/SortArraybyIncreasingFrequency.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class SortArraybyIncreasingFrequency {
+import java.util.*;
+
+public class SortArraybyIncreasingFrequency {
+    public static void main(String[] args) {
+        int[] arr={1,1,2,2,2,3};
+        System.out.println(Arrays.toString(frequencySort(arr)));
+
+    }
+    static int[] frequencySort(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+        for (int n : nums) {
+            ans.add(n);
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        Collections.sort(ans, (a, b) ->
+                (map.get(a) == map.get(b))? b - a : map.get(a) - map.get(b)
+        );
+
+        return ans.stream().mapToInt(i -> i).toArray();
+    }
+
 }
+
