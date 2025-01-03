b7d8d6171e10588ac18ae04d758086dba3df72c3
AMANPATEL <amanpatel8844382@gmail.com>
2025-01-03 07:28:20 +0530

    3Sum Closest Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/ThreeSumClosest.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/ThreeSumClosest.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class ThreeSumClosest {
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+    public static void main(String[] args) {
+            int[] arr={-1,2,1,-4};
+            int tar=1;
+        System.out.println(threeSumClosest(arr,tar));
+    }
+    static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                    closestSum = sum;
+                }
+
+                if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return closestSum;
+    }
 }
