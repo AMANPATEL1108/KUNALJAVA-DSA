8f3bbd778a706122035cf18e9bf405852eb92ca2
AMANPATEL <amanpatel8844382@gmail.com>
2025-01-06 08:08:47 +0530

    Insertion Sort List Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/InsertionSortList.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/InsertionSortList.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class InsertionSortList {
+public class InsertionSortList {
+    public static void main(String[] args) {
+        ListNode arr = new ListNode(4);
+        arr.next = new ListNode(2);
+        arr.next.next = new ListNode(1);
+        arr.next.next.next = new ListNode(3);
+
+        ListNode sortedList = insertionSortList(arr);
+
+        printList(sortedList);
+    }
+
+    static ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode helper = new ListNode(0);
+        ListNode cur = head;
+        ListNode pre = helper;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            while (pre.next != null && pre.next.val < cur.val) {
+                pre = pre.next;
+            }
+            cur.next = pre.next;
+            pre.next = cur;
+            pre = helper;
+            cur = next;
+        }
+
+        return helper.next;
+    }
+
+    static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
