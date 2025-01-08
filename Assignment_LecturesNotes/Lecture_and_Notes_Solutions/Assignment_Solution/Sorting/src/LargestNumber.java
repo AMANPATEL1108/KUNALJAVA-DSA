8b160f71f83be9e68eca2fa9561f765fcf810fa7
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-01-08 08:25:38 +0530

    Largest Number Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/LargestNumber.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Sorting/src/LargestNumber.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class LargestNumber {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int[] arr={3,30,34,5,9};
+        System.out.println(largestNumber(arr));
+    }
+    public static String largestNumber(int[] nums) {
+        String[] numStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            numStrs[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(numStrs, new Comparator<String>() {
+            public int compare(String a, String b) {
+                String ab = a + b;
+                String ba = b + a;
+                return ba.compareTo(ab);
+            }
+        });
+
+        if (numStrs[0].equals("0")) return "0";
+
+        String result = "";
+        for (String numStr : numStrs) {
+            result += numStr;
+        }
+
+        return result;
+    }
+
 }
