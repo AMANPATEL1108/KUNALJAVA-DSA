3ed6afc5e2a705cd6fe87122e90907e49ec0c21b
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-01-10 08:10:04 +0530

    Defanging an Ip address Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/DefanginganIpaddress.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/DefanginganIpaddress.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class DefanginganIpaddress {
+public class DefanginganIpaddress {
+    public static void main(String[] args) {
+        String a="1.1.1.1";
+        System.out.println(defangIPaddr(a));
+    }
+
+    public static String defangIPaddr(String address) {
+        String b = "";
+
+        for(int i=0;i<address.length();i++){
+            if (address.charAt(i)=='.'){
+                b+="[.]";
+            }else{
+                b+=address.charAt(i);
+            }
+        }
+        return b;
+    }
 }
