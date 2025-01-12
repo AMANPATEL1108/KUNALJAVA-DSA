107a6affb01ddcc24bba3e2ffecb56c98993135b
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-01-12 06:04:09 +0530

    Goal Parser Interpretation Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/GoalParserInterpretation.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/GoalParserInterpretation.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class GoalParserInterpretation {
+import java.util.ArrayList;
+
+public class GoalParserInterpretation {
+    public static void main(String[] args) {
+        String str="G()(al)";
+        System.out.println(interpret(str));
+    }
+    public static String interpret(String command) {
+        String newstr="";
+
+        ArrayList<Character> arrayList=new ArrayList();
+
+        for(int i=0;i< command.length();i++){
+            arrayList.add(command.charAt(i));
+        }
+
+        for(int i=0;i<command.length();i++){
+            if (arrayList.get(i) == '(' && arrayList.get(i+1)== ')' ){
+                    newstr+='o';
+            }else if(arrayList.get(i) != '(' && arrayList.get(i) != ')'){
+                    newstr+=arrayList.get(i);
+            }
+
+
+        }
+
+
+        return newstr;
+    }
 }
