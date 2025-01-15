313b3734c1fa18439c8ff4c06b3e83dcbb905780
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-01-15 07:24:23 +0530

    To Lower Case Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/ToLowerCase.java
@@ -0,0 +1,10 @@
+public class ToLowerCase {
+    public static void main(String[] args) {
+    String str="Hello";
+        System.out.println(toLowerCase(str));
+    }
+    public static String toLowerCase(String s) {
+
+        return s.toLowerCase();
+    }
+}
