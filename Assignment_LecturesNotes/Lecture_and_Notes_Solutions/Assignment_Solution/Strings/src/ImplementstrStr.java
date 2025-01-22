86c517cb9b4f5bbbf92dd09d7704b98c5ad63908
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-01-22 20:06:36 +0530

    Implement strStr Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/ImplementstrStr.java
@@ -0,0 +1,16 @@
+public class ImplementstrStr {
+    public static void main(String[] args) {
+//        haystack = "sadbutsad", needle = "sad"
+        String s1="sadbutsad";
+        String s2="sad";
+        System.out.println(strStr(s1,s2));
+    }
+    public static  int strStr(String haystack, String needle) {
+
+        if (haystack.contains(needle)) {
+            return haystack.indexOf(needle);
+        }
+        return -1;
+    
+    }
+}
