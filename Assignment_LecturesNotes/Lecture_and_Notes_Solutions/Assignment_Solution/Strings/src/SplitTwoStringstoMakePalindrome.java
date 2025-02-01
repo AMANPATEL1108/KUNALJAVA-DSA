50d66fa5940020d34fd619c853174cb9ab06cf26
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-02-01 05:38:04 +0530

    Split Two Strings to Make Palindrome Completed

--- Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/SplitTwoStringstoMakePalindrome.java
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/SplitTwoStringstoMakePalindrome.java
@@ -1,36 +1,32 @@
 public class SplitTwoStringstoMakePalindrome {
     public static void main(String[] args) {
-        String a="x";
-        String b="y";
-        System.out.println(checkPalindromeFormation(a,b));
+        String a = "x";
+        String b = "y";
+        System.out.println(checkPalindromeFormation(a, b));
     }
 
-    public static boolean checkPalindromeFormation(String a, String b) {
-        return solve(a,b) || solve(b,a);
-    }
 
-    private static boolean solve(String a, String b){
-        int i = 0;
-        int j = b.length()-1;
-
-        while(a.charAt(i) == b.charAt(j)){
-            i++;
-            j--;
+        public static boolean checkPalindromeFormation(String a, String b) {
+            return split(a, b) || split(b, a);
         }
 
-        String ABB = a.substring(0,i) + b.substring(i);
-        String AAB = a.substring(0,a.length()-i) + b.substring(b.length()-i);
-
-        return isPalindrome(ABB) || isPalindrome(AAB);
-    }
-
-    private static boolean isPalindrome(String s){
-        int n = s.length();
-        for(int i=0;i<n/2;i++){
-            if(s.charAt(i) != s.charAt(n-i-1)){
-                return false;
+        private static boolean split(String a, String b) {
+            int left = 0, right = a.length() - 1;
+            while (left < right && a.charAt(left) == b.charAt(right)) {
+                left++;
+                right--;
             }
+            if (left >= right) return true;
+            return isPalindrome(a, left, right) || isPalindrome(b, left, right);
         }
-        return true;
+
+        private static boolean isPalindrome(String s, int left, int right) {
+            while (left <= right) {
+                if (s.charAt(left) != s.charAt(right)) return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
     }
-}
+
