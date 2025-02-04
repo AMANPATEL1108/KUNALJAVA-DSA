aab191dcc23ddbbf1140b7c73036465569e7522d
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-02-04 19:25:42 +0530

    Repeated String Match Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/RepeatedStringMatch.java
@@ -0,0 +1,30 @@
+public class RepeatedStringMatch {
+    public static void main(String[] args) {
+        String a="abcd";
+        String b="cdabcdab";
+        System.out.println(repeatedStringMatch(a,b));
+    }
+
+    public static int repeatedStringMatch(String a, String b) {
+        StringBuilder s = new StringBuilder(a);
+        int count = 1;
+
+        while (s.length() < b.length()) {
+            s.append(a);
+            count++;
+        }
+
+        if (s.indexOf(b) != -1) {
+            return count;
+        }
+
+        s.append(a);
+        count++;
+
+        if (s.indexOf(b) != -1) {
+            return count;
+        }
+
+        return -1;
+    }
+}
