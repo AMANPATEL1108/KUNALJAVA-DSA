5df7ddecff38ff636c89f74313c3d79a287d40f7
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-02-11 19:48:43 +0530

    Basic Calculator II Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Strings/src/BasicCalculatorII.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class BasicCalculatorII {
+    public static void main(String[] args) {
+    String s="3+2*2";
+        System.out.println(calculate(s));
+    }
+
+    public static int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int len = s.length();
+        int num = 0;
+        char sign = '+';
+        int res = 0;
+        for(int i = 0; i< len; i++) {
+            char crrnum = s.charAt(i);
+            if(Character.isDigit(s.charAt(i))){
+                num = num * 10 + crrnum - '0';
+            }
+            if(!Character.isDigit(crrnum) && crrnum != ' ' || i == len-1) {
+                if(sign == '+') stack.push(num);
+                if(sign == '-') stack.push(-num);
+                if(sign == '*') stack.push(stack.pop() * num);
+                if(sign == '/') stack.push(stack.pop() / num);
+                sign = s.charAt(i);
+                num = 0;
+            }
+        }
+        for(int i : stack) {
+            res += i;
+        }
+        return res;
+    }
+}
