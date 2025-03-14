58cd29a4dc88957ba1d56e907bf41d0ec379e918
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-03-14 07:51:26 +0530

    Perfect squares Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Recursion/src/Perfectsquares.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Perfectsquares {
+    public static void main(String[] args) {
+        int a=12;
+        System.out.println(numSquares(a));
+
+    }
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        int count = 1;
+        while (count * count <= n) {
+            int sq = count * count;
+            for (int i = sq; i <= n; i++) {
+                dp[i] = Math.min(dp[i - sq] + 1, dp[i]);
+            }
+            count++;
+        }
+        return dp[n];
+    }
+
+
+}
