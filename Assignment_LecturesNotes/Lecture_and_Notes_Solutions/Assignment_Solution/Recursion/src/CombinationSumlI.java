be42ddf088b8977d3ee3b4cd9cbda4a5235b3cb9
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-03-20 07:49:14 +0530

    Combination Sum II Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Recursion/src/CombinationSumlI.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumlI {
+    public static void main(String[] args) {
+        int[] a={10,1,2,7,6,1,5};
+        int b=8;
+        System.out.println(combinationSum2(a,b));
+    }
+
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+
+        dfs(candidates, target, 0, new ArrayList<Integer>(), res);
+        return res;
+    }
+
+    private static void dfs(int[] candidates, int target, int start, List<Integer> comb, List<List<Integer>> res) {
+        if (target < 0) {
+            return;
+        }
+
+        if (target == 0) {
+            res.add(new ArrayList<Integer>(comb));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i-1]) {
+                continue;
+            }
+
+            if (candidates[i] > target) {
+                break;
+            }
+
+            comb.add(candidates[i]);
+            dfs(candidates, target - candidates[i], i + 1, comb, res);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
