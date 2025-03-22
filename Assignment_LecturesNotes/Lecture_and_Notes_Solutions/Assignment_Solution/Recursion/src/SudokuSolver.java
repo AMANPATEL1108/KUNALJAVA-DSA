9e9884e7f7a43bf7009f7d7a66162aa3b63a1160
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-03-22 07:09:58 +0530

    Sudoku Solver Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Recursion/src/SudokuSolver.java
@@ -0,0 +1,70 @@
+public class SudokuSolver {
+    public static void main(String[] args) {
+        char board[][] = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+      solveSudoku(board);
+    }
+
+
+    public static void solveSudoku(char[][] board) {
+        solveSudoku(board, 0, 0);
+    }
+
+    private static boolean solveSudoku(char[][] board, int row, int col) {
+        if (col == 9) {
+            col = 0;
+            row++;
+            if (row == 9) {
+                return true;
+            }
+        }
+
+        if (board[row][col] != '.') {
+            return solveSudoku(board, row, col + 1);
+        }
+
+        for (char c = '1'; c <= '9'; c++) {
+            if (isValid(board, row, col, c)) {
+                board[row][col] = c;
+                if (solveSudoku(board, row, col + 1)) {
+                    return true;
+                }
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) {
+                return false;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c) {
+                return false;
+            }
+        }
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
