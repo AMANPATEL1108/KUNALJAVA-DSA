99c88eaaa69d7f31531d2b8c54eef4d7cfde9443
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-03-30 08:07:26 +0530

    Permutation Sequence Complpeted

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Recursion/src/PermutationSequence.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class PermutationSequence {
+    public static void main(String[] args) {
+    int a=3;
+    int b=3;
+
+        System.out.println(getPermutation(a,b));
+    }
+
+    public static String getPermutation(int n, int k) {
+        List<Integer> num = new LinkedList<Integer>();
+        for (int i = 1; i <= n; i++) num.add(i);
+        int[] fact = new int[n];  // factorial
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) fact[i] = i*fact[i-1];
+        k = k-1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i > 0; i--){
+            int ind = k/fact[i-1];
+            k = k%fact[i-1];
+            sb.append(num.get(ind));
+            num.remove(ind);
+        }
+        return sb.toString();
+    }
+}
