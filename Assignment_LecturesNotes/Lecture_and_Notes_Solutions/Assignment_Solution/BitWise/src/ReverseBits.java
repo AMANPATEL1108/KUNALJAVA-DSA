a15bb16c9da26821cf2aa56c9b291b96e61ea6b0
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-04-09 07:50:50 +0530

    Reverse Bits Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/BitWise/src/ReverseBits.java
@@ -0,0 +1,16 @@
+public class ReverseBits {
+    public static void main(String[] args) {
+    int a=00000010100101000001111010011100;
+        System.out.println(reverseBits(a));
+    }
+
+    public static int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;
+            result = (result << 1) | bit;
+            n = n >>> 1;
+        }
+        return result;
+    }
+}
