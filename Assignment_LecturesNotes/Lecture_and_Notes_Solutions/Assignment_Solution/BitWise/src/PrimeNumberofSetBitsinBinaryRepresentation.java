d6babf97eaab61b0648c3b960632f910dfeebc76
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-04-16 07:38:55 +0530

    Prime Number of Set Bits in Binary Representation Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/BitWise/src/PrimeNumberofSetBitsinBinaryRepresentation.java
@@ -0,0 +1,48 @@
+public class PrimeNumberofSetBitsinBinaryRepresentation {
+    public static void main(String[] args) {
+        int left=6;
+        int right=10;
+        System.out.println(countPrimeSetBits(left,right));
+    }
+
+    public static int countPrimeSetBits(int left, int right) {
+        int ans=0;
+        int c=0;
+        boolean res=false;
+
+        for(int i=left;i<=right;i++){
+            ans=hammingWeight(i);
+
+            res=isPrime(ans);
+
+            if(res==true){
+                c++;
+            }
+        }
+        return c;
+
+    }
+    public static int hammingWeight(int n) {
+        int c=0;
+        while(n>0){
+            if((n & 1)==1){
+                c++;
+            }
+            n=n>>1;
+
+        }
+        return c;
+    }
+
+    static boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+}
