2f1d24aeb9aae8fa971382d6156fe98a04c1459a
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-05-05 06:55:43 +0530

    Integer to Roman Completed

---
+++ Assignment_LecturesNotes/Lecture_and_Notes_Solutions/Assignment_Solution/Math/src/IntegertoRoman.java
@@ -0,0 +1,14 @@
+public class IntegertoRoman {
+    public static void main(String[] args) {
+    int n=3749;
+        System.out.println(intToRoman(n));
+    }
+
+    public static  String intToRoman(int num) {
+        String M[] = { "", "M", "MM", "MMM" };
+        String C[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        String X[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        String I[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
+    }
+}
