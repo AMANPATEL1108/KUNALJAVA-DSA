f9c4c3b74cf04f45d73bd8c9fbbfdced08a1128c
AMANPATEL1108 <amanpatel8844382@gmail.com>
2025-05-20 10:31:22 +0530

    Linked List Singly,Circular,Doubly Linked List Completed

--- LinkedList_Tutorial_Singly_Doubly_Circular_Theory_Code_Implementation/src/DoublyLinkedList.java
+++ LinkedList_Tutorial_Singly_Doubly_Circular_Theory_Code_Implementation/src/DoublyLinkedList.java
@@ -1,4 +1,103 @@
-package PACKAGE_NAME;
+import com.sun.jmx.snmp.SnmpNull;
 
 public class DoublyLinkedList {
+
+    private Node head;
+
+
+    public Node find(int val) {
+        Node node = head;
+        while (node != null) {
+            if (node.val == val) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void insertd(int after, int val) {
+        Node p = find(after);
+        if (p == null) {
+            System.out.println("Doest Exisr");
+            return;
+        }
+        Node node = new Node(val);
+        node.next = p.next;
+        p.next = node;
+        node.prev = p;
+        if (node.next != null) {
+            node.next.prev = node;
+        }
+    }
+
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        node.prev = null;
+        if (head != null) {
+            head.prev = node;
+        }
+        head = node;
+
+    }
+
+    public void insertLast(int val) {
+        Node node = new Node(val);
+        Node last = head;
+
+        node.next = null;
+
+        if (head == null) {
+            node.prev = null;
+            head = null;
+            head = node;
+            return;
+        }
+
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = node;
+        node.prev = last;
+
+    }
+
+
+    public void display() {
+        Node last = null;
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.val + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+
+        System.out.println("print in Reverser");
+        while (last != null) {
+            System.out.print(last.val + " -> ");
+            last = last.prev;
+        }
+        System.out.println("Start");
+    }
+
+
+    private class Node {
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node next, Node prev) {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
 }
--- LinkedList_Tutorial_Singly_Doubly_Circular_Theory_Code_Implementation/src/Main.java
+++ LinkedList_Tutorial_Singly_Doubly_Circular_Theory_Code_Implementation/src/Main.java
@@ -1,20 +1,41 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        LL list = new LL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertLast(20);
-        list.insert(4,3);
-        list.display();
-        list.deleteByIndex(2);
+//      ------------------  //single linked list
+//        SinglyLinedList list = new SinglyLinedList();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(20);
+//        list.insert(4,3);
+//        list.display();
+//        list.deleteByIndex(2);
 //        list.deleteLast();
+//        list.display();
+
+        //---------------------//doubly Linked List
+//
+//        DoublyLinkedList list = new DoublyLinkedList();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insertd(8, 65);
+//        list.display();
+
+        //Circular Linked List-----------------
+
+        CircularLinkedList list = new CircularLinkedList();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+
         list.display();
-
-
+        list.delete(19);
+        list.display();
 
     }
 }
